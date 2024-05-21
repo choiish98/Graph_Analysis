7821aa0a868dd5f2f49358cbd89bec550b4f67ba
choiish98 <one9119@unist.ac.kr>
2024-05-21 08:43:51 +0000

    first commit (gnuplot + matplot)

---
+++ matplot2.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("start")
+
+data = []
+file_path = "stream_minor"
+with open(file_path, "r") as file:
+    for line in file:
+        pair = tuple(map(float, line.split()))
+        data.append(pair)
+
+print("getting data done")
+
+grouped_data = {}
+for key, value in data:
+    if value > 300 or value == 0:
+        continue
+    if key not in grouped_data:
+        grouped_data[key] = []
+    grouped_data[key].append(value)
+
+print("data processing done")
+
+for i in range(7):
+    key_value = 0
+    values = grouped_data[key_value]
+
+#x = np.arange(1, len(values) + 1) / len(values) * 100
+    x = np.arange(1, len(values) / 1000)
+    indx = int(len(values) / 1000)
+    y = values[indx:indx*2]
+
+    plt.plot(x, y)
+
+print("making plot done")
+print("now you can see plot chart")
+
+plt.xlabel('timeline')
+plt.ylabel('Value')
+plt.title(f'Stream Application Minor Fault')
+plt.grid(True)
+plt.show()
+
